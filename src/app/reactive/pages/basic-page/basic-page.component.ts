d0caddee6b4a966f5bee8e1b40709ca759cba254
Duvan <duvanriano2626@gmail.com>
2024-08-16 14:28:56 -0500

    Enlazar formularios reactivos al template html recuerde importar el en module ReactiveFormsModule para que pueda funcionar el formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './basic-page.component.css'
 })
 export class BasicPageComponent {
+
+  // public myForm:FormGroup = new FormGroup({
+  //   name: new FormControl('',[],[]),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // })
+
+  public myForm:FormGroup = this.fb.group({
+    name: [''],
+    price:[0],
+    inStorage:[0]
+  })
+
+
+  constructor(private fb:FormBuilder){
+
+  }
+
+  public onSave = ():void => {
+    console.log(this.myForm.value);
+
+  }
 
 }
