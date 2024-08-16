47779c07f69148d74b4a85aaadc34848ada2717f
Duvan <duvanriano2626@gmail.com>
2024-08-16 15:40:11 -0500

    Creacion de metodos para mejorar la legibilidad del codigo en la muestra de errores

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -15,6 +15,7 @@ const rtx5090 = {
 })
 export class BasicPageComponent implements OnInit {
 
+
   // public myForm:FormGroup = new FormGroup({
   //   name: new FormControl('',[],[]),
   //   price: new FormControl(0),
@@ -36,15 +37,44 @@ export class BasicPageComponent implements OnInit {
     // this.myForm.reset(rtx5090);
   }
 
+
+  public isValidField = (field:string):boolean | null => {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched
+  }
+
+  public getFieldError = (field:string): string | null  => {
+
+    if ( !this.myForm.contains(field) ) {
+      return null
+    }
+
+    const errors = this.myForm.controls[field].errors || {};
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Minimo debe tener ${errors['minlength'].requiredLength} caracteres `
+      }
+    }
+
+    return null;
+
+  }
+
+
+
   public onSave = ():void => {
 
     if(this.myForm.invalid){
       this.myForm.markAllAsTouched();
       return
     }
-
     console.log(this.myForm.value);
     this.myForm.reset({price:0, inStorage:0})
   }
+
+
+
 
 }
