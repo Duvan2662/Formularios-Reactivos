caeee99870480f8944cb73c2a58288179eb55e28
Duvan <duvanriano2626@gmail.com>
2024-08-17 13:13:48 -0500

    ojo con este commit aqui se ve reflejado como comparar dos correos desde un backen para saber si ya eiste o no pendiente este comentado

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
-import { delay, Observable, of } from 'rxjs';
+import { delay, map, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +9,53 @@ import { delay, Observable, of } from 'rxjs';
 export class EmailValidatorService implements AsyncValidator{
 
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
+
     const email = control.value;
     console.log({email});
 
-    return of({
-      emailTaken:true
+    const httpCallObservable = new Observable<ValidationErrors | null> ((subscriber) =>{
+      console.log({email});
+      if (email === 'duvanriano2015@gmail.com') {
+        subscriber.next({emailtaken:true});
+        subscriber.complete();
+        return
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
+
     }).pipe(
-      delay(200)
+      delay(3000)
     )
+
+    return httpCallObservable
+
   }
+
+
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //   const email = control.value;
+  //   console.log({email});
+
+  //   return of({
+  //     emailTaken:true
+  //   }).pipe(
+  //     delay(200)
+  //   )
+  // }
+
+
+  //Peticion a un backen o un servicio
+
+  // return this.http.get<any[]>(`http://localhost:3000/users?q=${email}`)
+  //     .pipe(
+  //       map(res => {
+  //         return (res.length ===0)
+  //           ? null
+  //           :{emailTaken:true}
+  //       })
+  //     )
 
 
 }
