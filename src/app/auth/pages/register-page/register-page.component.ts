99f6003d403953dbd75145a78f50f3f9728d0e6f
Duvan <duvanriano2626@gmail.com>
2024-08-17 12:02:47 -0500

    Coneion del template a nuestro formulario de registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+
+  public myform: FormGroup = this.fb.group({
+    name:['',[Validators.required]],
+    email:['',[Validators.required]],
+    username:['',[Validators.required]],
+    password:['',[Validators.required, Validators.minLength(6)]],
+    password2: ['',[Validators.required]]
+  });
+
+
+  constructor(private fb:FormBuilder) {}
+
+  public isValidField = (field:string) => {
+    //TODO:Obtener validacion desde un servicio
+  }
+
+  public onSubmit = () => {
+    this.myform.markAllAsTouched();
+  }
 
 }
